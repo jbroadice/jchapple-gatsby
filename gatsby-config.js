54523bc76acdf68229dcbf0934d790a75da90a87
Jack Chapple <jchapple@gmail.com>
2020-05-07 07:39:57 +0100

    Minor initial changes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Jack Chapple - Composer and Pianist',
+    author: 'Jack Chapple',
+    description: 'The official website of composer and pianist Jack Chapple.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
